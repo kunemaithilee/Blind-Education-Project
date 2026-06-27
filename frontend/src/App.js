af6b7ca0193b99160fe3207f577302c3717b32de
Onkar Nagul <onkarnagul678@gmail.com>
2026-06-27 15:15:38 +0530

    Revert "Add footer with EcoFuel Connect & TechFix Store links"
    This reverts commit bdc85821da3a0cc4d817333a17b1d6a5c675cc6d.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { VoiceProvider } from "./context/VoiceContext";
 import VoiceAssistant from "./components/voice/VoiceAssistant";
-import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import "./App.css";
 
@@ -9,12 +8,7 @@ function App() {
   return (
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <VoiceProvider>
-        <div className="app-layout">
-          <div className="app-content">
-            <AppRoutes />
-          </div>
-          <Footer />
-        </div>
+        <AppRoutes />
         <VoiceAssistant />
       </VoiceProvider>
     </BrowserRouter>
